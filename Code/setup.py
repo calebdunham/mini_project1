23992e3ba64df5546b608713737fa07214bd1526
calebdunham <50708163+calebdunham@users.noreply.github.com>
2021-10-02 23:26:01 -0700

    Added setup.py

---
+++ Code/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='mini_project1',
+    version='1.0',
+    packages=[''],
+    package_dir={'': 'Code'},
+    url='https://github.com/calebdunham/mini_project1.git',
+    license='',
+    author='Caleb Dunham',
+    author_email='calebd146@gamil.com',
+    description='Python OOP Mini-Project'
+)
